3968ea8a27d90320e0d30236ab11062b87b9d88c
Gulnaz Ahmad <130208290+killua0393@users.noreply.github.com>
2025-07-25 01:40:17 +0530

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: true, // or wherever your frontend is
+    origin: "https://gravity-to-do-app.onrender.com", // or wherever your frontend is
     credentials: true,
   })
 );
